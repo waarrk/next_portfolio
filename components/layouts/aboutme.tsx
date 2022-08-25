2cd55901f2fdb142fea933636793dac7ece843e5
waarrk <washioyusaku@icloud.com>
2022-08-25 13:12:42 +0900

    add: responsive

--- components/layouts/aboutme.tsx
+++ components/layouts/aboutme.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Aboutme() {
   return (
     <>
-        <div className='max-w-sm mx-auto flex py-8 px-10 rounded-lg shadow-neumorphismdark'>
+        <div className='max-w-xl min-w-xl mx-auto flex py-8 px-10 mt-10 lg:mt-0 rounded-lg shadow-neumorphismdark'>
             <Image src="/images/kamo.jpg" width={90} height={90} objectFit="contain" className='rounded-full' />
             <div className="ml-6 pt-1">
                 <p className="text-2xl leading-tight">Yusaku Washio</p>
