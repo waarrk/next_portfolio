747a0cb0087c1d934723081597ed748fb70c440c
waarrk <washioyusaku@icloud.com>
2022-08-29 12:03:37 +0900

    add: aboutme

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,14 @@ const Home: NextPage = () => {
       <main className='min-h-screen pt-20 lg:pt-24 items-start flex justify-center'>
         <div className='w-10/12 max-w-5xl lg:flex lg:justify-around'>
 
-          <div className='max-w-xl  lg:max-w-none lg:w-3/6 mx-auto flex py-8 px-10 rounded-lg shadow-neumorphismdark'>
-            <p>This is waarrk's personal page.</p>
+          <div className='max-w-xl  lg:max-w-none lg:w-3/6 mx-auto py-8 px-10 rounded-lg shadow-neumorphismdark'>
+            <p className="text-xl leading-tight"> # ■ About me</p>
+            <p className='text-base leading-normal text-gray-300 mt-2'>
+              I am a Japanese ( Niigata, Nagaoka ) Software Programmer and also Electronic Circuit Designer.
+            </p>
+            <p className='text-base leading-normal text-gray-300 mt-1'>
+              I have knowledge of embedded system development using C++, Eagle, etc., simple smartphone application development, and web/network.
+            </p>
           </div>
           
           <Aboutme/>
