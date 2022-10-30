8abefd71075ab873a84e071492a71039cbcc6555
waarrk <washioyusaku@icloud.com>
2022-10-30 11:37:57 +0900

    modify : Appディレクトリの使用  - #none: Next13の新機能Appディレクトリ使用に変更

--- __test__/footer.test.tsx
+++ __test__/footer.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Footer from '../components/layouts/footer'
+import Footer from '../components/organisms/Footer'
 
 describe('Rendering', () => {
   it('Should render text', () => {
